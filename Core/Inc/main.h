001c0bf335ee50b9d118171e0a904bf2e786b7a7
AhmedRouatbi <rouetbii.ahmed1@gmail.com>
2022-02-21 20:35:42 +0100

    added basic code running on Clion

---
+++ Core/Inc/main.h
@@ -0,0 +1,7 @@
+
+#ifndef __MAIN_H
+#define __MAIN_H
+#include "gpio.h"
+void Error_Handler(void);
+
+#endif
---
+++ Core/Src/main.c
@@ -0,0 +1,30 @@
+
+#include "main.h"
+
+void nothing()
+
+{
+  for(uint16_t i=0;i<UINT64_MAX;i++)
+  {
+
+  }
+
+}
+
+int main(void) {
+
+  while (1)
+  {
+    nothing();
+  }
+
+}
+
+void Error_Handler(void) {
+
+  while (1)
+  {
+  }
+
+}
+
